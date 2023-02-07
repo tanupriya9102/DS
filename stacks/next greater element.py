d70ba38127091b77defe517a0b5648c092ee3d7c
Tanupriya Pathak <87934620+tanupriya9102@users.noreply.github.com>
2023-02-07 09:20:33 +0530

    Create next greater element.py

---
+++ stacks/next greater element.py
@@ -0,0 +1,28 @@
+arr=[1,3,2,4]
+s=[]
+v=[]
+
+
+
+for i in reversed(range(len(arr))):
+    if len(s)==0:
+        v.append(-1)
+        s.append(arr[i])
+    else:
+        if(arr[i]<s[0]):
+            v.append(s[0])
+            s.append(arr[i])
+        else:
+            while(arr[i]>=s[0]):
+                s.pop()
+            v.append(s[0])
+            s.append(arr[i])
+
+print(s)
+print(v)
+            
+                
+            
+            
+        
+    
